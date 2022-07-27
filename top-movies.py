3b699ae995803341bf23e77344515fe7f5b474cb
Patryk <potryk4us@gmail.com>
2022-07-27 14:24:32 +0200

    Before reducing container variable.

--- top-movies.py
+++ top-movies.py
@@ -8,3 +8,17 @@ service = Service(executable_path=path)
 driver = webdriver.Chrome(service=service)
 
 driver.get(website)
+####################################
+
+# used single quota and we can use different by than xpath
+containers = driver.find_element(by="xpath", value='//div[@class="movies-list-item"]/div[@class="info-list"]/div[@class="sum-vote"]')
+
+driver.find_element(by="xpath", value='//div[@class="movies-list-item"]/div[@class="info-list"]/div[@class="sum-vote"]/div/text()')
+
+
+
+
+# xpath
+# //div[@class="movies-list-item"]/div[@class="info-list"]/div[@class="sum-vote"]
+# //div[@class="movies-list-item"]/div[@class="info-list"]/div[@class="sum-vote"]/div/text()
+# //div[@class="movies-list-item"]/div[@class="opis-list"]/div[@class="title"]/a/text()
