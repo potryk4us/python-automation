2cf5f43be20547125a7764bc2f1090dcf462be3a
Patryk <potryk4us@gmail.com>
2022-07-31 22:24:09 +0200

    Added headless mode.

--- top-movies.py
+++ top-movies.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
 
 website = "https://ekino-tv.pl/movie/cat/+kategoria[17]+wersja[Lektor]+/"
 path = 'C:/Users/potrykus/Downloads/chromedriver.exe'
 
-service = Service(executable_path=path)
-driver = webdriver.Chrome(service=service)
+# headless-mode
+options = Options()
+options.headless = True
 
+service = Service(executable_path=path)
+driver = webdriver.Chrome(service=service, options=options)
 driver.get(website)
 ####################################
 
@@ -29,20 +33,20 @@ links = []
 for container in containers:
 #    print(container.find_element(by="xpath", value='//div[@class="movies-list-item"]/div[@class="info-list"]/div[@class="sum-vote"]/div').text)
 #    driver.find_element(by="xpath", value='//div[@class="movies-list-item"]/div[@class="info-list"]/div[@class="sum-vote"]/div/text()')
-    sum_vote = container.find_element(by="xpath", value='./div[@class="info-list"]/div[@class="sum-vote"]/div').text
     title = container.find_element(by="xpath", value='./div[@class="opis-list"]/div[@class="title"]/a').text
+    sum_vote = container.find_element(by="xpath", value='./div[@class="info-list"]/div[@class="sum-vote"]/div').text
     link = container.find_element(by="xpath", value='./div[@class="opis-list"]/div[@class="title"]/a').get_attribute("href")
 #
  #   # appending each element to the list
-    sum_votes.append(sum_vote)
     titles.append(title)
+    sum_votes.append(sum_vote)
     links.append(link)
 
 # dictionary consist of a key and a key value pair.
-my_dict = {'title': titles, 'ocena': sum_votes, 'url': links }
+my_dict = {'title': titles, 'vote': sum_votes, 'url': links}
 
 df_headlines = pd.DataFrame(my_dict)
-df_headlines.to_csv('headlines.csv')
+df_headlines.to_csv('headlines-headless.csv')
 
 driver.quit()
 # xpath
